98c3c91828ac6e116b6a4fce2a98ff31cfd7627b
Sanne Helvig <sdhelvig@stud.ntnu.no>
2020-04-12 00:38:57 +0200

    Now works on the inter of webs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// var http = require('http');
 const bodyParser = require("body-parser");
 var MongoClient = require('mongodb');
 var cors = require('cors');
@@ -12,6 +13,7 @@ app.use(
     })
 );
 app.use(bodyParser.json());
+
 
 // Configure Mongo
 const dbURL = "mongodb://localhost:27017/";
@@ -29,10 +31,22 @@ MongoClient.connect(dbURL, function(err, dbclient) {
   });
 
 // Make sure connections are accepted from frontend
-app.use(cors({origin: 'http://kjipest.surge.sh'}));
+// app.use(cors({origin: 'http://kjipest.surge.sh'}));
+app.use(cors());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 // Set up GET routine
-app.get("/sorted/", (req, res) => {
+app.get("/", (req, res) => {
+    // res.send(JSON.stringify('success'));
+    // res.send('')
+    console.log("Got connection!");
     // // Connect to Mongo 
     MongoClient.connect(dbURL, function(err, dbclient) {
     if (err) {throw err;}
@@ -54,7 +68,7 @@ app.get("/sorted/", (req, res) => {
 
 
 // Specify the Port where the backend server can be accessed and start listening on that port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8888;
 // Set up tunnel from https://kjipest.localtunnel.me to https://localhost:5000
 // const tunnel = localtunnel(port, { subdomain: 'kjipest'}, (err, tunnel) => {
 //     if (err) console.log(err)
@@ -72,7 +86,9 @@ const port = process.env.PORT || 5000;
 //         console.log("Tunnel closed!")
 //       });
 //   })();
+// var httpServer = http.createServer(app);
 const hostname = '0.0.0.0';
-app.listen(port, hostname, () => console.log(`Server up and running on port ${port}.`));
+app.listen(port,hostname,() => console.log(`Server up and running on port ${port}.`));
+// app.listen(port, hostname, () => console.log(`Server up and running on port ${port}.`));
 
 
